8347383d3c44ad1ba901d57fd4f6240f48933a03
avi-2-avi <avicon95@protonmail.com>
2023-02-19 16:13:19 -0500

    Register note and admin note controls

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    tittle = models.CharField(null=True, max_length=50)
+    body = models.TextField(null=True, blank=True, max_length=200)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "notes"
