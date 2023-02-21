a3db5a8f42438681cfbe75ccfee305de7f1965d8
avi-2-avi <avicon95@protonmail.com>
2023-02-21 16:42:59 -0500

    Serialized views - to be Class-based views in a future

--- api/views.py
+++ api/views.py
@@ -1,39 +1,41 @@
-from rest_framework.decorators import api_view 
+from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Note
 from .serializers import NoteSerializer
-
-# class NoteView(viewsets.ModelViewSet):
-#     queryset = Note.objects.all()
-#     serializer_class = NoteSerializer
-
-    
-    # def get(self, request, id):
-    #     note = Note.objects.get(id=id)
-    #     return views.Response(note)
-
-    # def get_all(self, request):
-    #     notes = Note.objects.order_by('updated')
-    #     return HttpResponse(notes)
-
-    # def post(self, request):
-    #     data = request.data
-    #     new_note = Note.objects.create(
-    #         tittle=data['tittle'],
-    #         description=data['description']
-    #     )
 
 @csrf_exempt
 def note_list(request):
     if request.method == 'GET':
-        notes = Note.objects.all()
+        notes = Note.objects.all().order_by('-updated')
         serializer = NoteSerializer(notes, many=True)
         return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = NoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
 def note_detail(request, id, format=None):
+    note = Note.objects.get(id=id)
+
     if request.method == 'GET':
-        note = Note.objects.get(id=id)
         serializer = NoteSerializer(note)
         return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = NoteSerializer(note, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        note.delete()
+        return HttpResponse(status=204)
+
+
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'notesdb',
-        'USER': 'user',
-        'PASSWORD': 'pass',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432'
     }
 }
