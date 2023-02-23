885ea6d892f3d09dfc20cc48eaebe8b3c5bcd453
avi-2-avi <avicon95@protonmail.com>
2023-02-23 01:35:52 -0500

    I'm a dumb dumb

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Note(models.Model):
     tittle = models.CharField(null=True, max_length=50)
     body = models.TextField(null=True, blank=True, max_length=200)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "notes"
